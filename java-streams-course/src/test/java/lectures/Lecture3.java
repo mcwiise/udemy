0711ad93a48c64e0c895219bfe681a857c9d6554
Captain Mcwiise <mcwiise.cap@gmail.com>
2024-05-06 10:16:46 -0500

    implementing loops, min max and comparators witn streams

--- java-streams-course/src/test/java/lectures/Lecture3.java
+++ java-streams-course/src/test/java/lectures/Lecture3.java
@@ -12,12 +12,14 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    Integer min = numbers.stream().min(Comparator.naturalOrder()).get();
+    assertThat(1).isEqualTo(min);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    Integer max = numbers.stream().max(Comparator.naturalOrder()).get();
+    assertThat(100).isEqualTo(max);
   }
 }
--- java-streams-course/src/test/java/lectures/Lecture5.java
+++ java-streams-course/src/test/java/lectures/Lecture5.java
@@ -18,14 +18,18 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
-
+    Predicate<Car> filterPredicate = car -> car.getPrice() < 1000;
+    List<Car> carsUnder1000 = cars.stream()
+            .filter(filterPredicate)
+            .collect(Collectors.toList());
+    carsUnder1000.stream().forEach(car -> System.out.println(car));
   }
 
   @Test
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
-
+    
   }
 
   @Test
